c7260ff18846b3f4cd3a6889661310b136fa9946
Zebio <mateuseusebio@gmail.com>
2021-04-25 23:03:41 -0300

    Eliminação do Ruído e aumento na precisão da leitura do ADC

---
+++ Readme.md
@@ -0,0 +1,4 @@
+https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/adc.html#adc-calibration
+
+ADC VRef calibration: 1114mV
+
--- src/main.c
+++ src/main.c
@@ -1,63 +1,57 @@
+/*-------------------Bibliotecas de projeto----------------------*/
+
+#include <stdio.h>      //printf
 #include <driver/adc.h> //biblioteca do ADC
 #include <driver/uart.h>//biblioteca do UART
-#include <string.h>     //strlen
+#include "esp_adc_cal.h"
 
 
 
-void setup_ADC();
-void setup_UART();
+/*-------------------Mapeamento de Hardware----------------------*/
+#define pino_de_leitura ADC1_CHANNEL_5 //Pino escolhido para leitura do adc: ADC1-CH5 = GPIO33
+
+
+/*-------------------Constantes de Projeto ----------------------*/
+const uint32_t defaultVref=1114; //Tensão de Referência do meu ESP32 =1.114 V
+//Mais informações em: https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/adc.html#adc-calibration
+
+
+
+esp_adc_cal_characteristics_t adc_config;//struct para uso da função de calibração: esp_adc_cal_characterize();
+//mais informações em: https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/adc.html#_CPPv424esp_adc_cal_characterize10adc_unit_t11adc_atten_t16adc_bits_width_t8uint32_tP29esp_adc_cal_characteristics_t
+
+/*-------------------Declaração das Funções----------------------*/
+void setup_ADC(); //Configurações iniciais do ADC
 
 void app_main() {
     setup_ADC();
-    setup_UART();
     while (1)
     {
-        int val = adc1_get_raw(ADC1_CHANNEL_0); //captura o valor de tensão do GPIO36.
+        //É guardado na variável reading o valor bruto lido pelo adc
+        uint32_t reading =  adc1_get_raw(pino_de_leitura); 
 
-        char* test_str = "This is a test string.\n";
-        uart_write_bytes(UART_NUM_0, (const char*)test_str, strlen(test_str));
+        //Usamos essa função para converter o valor bruto do ADC para um valor em Tensão.
+        //Se tentarmos converter o valor sem essa função podemos não ler um valor preciso
+        //Para chamá-la é necessário primeiro configurar o ADC como foi feito na função
+        //esp_adc_cal_characterize()
+        uint32_t voltage =  esp_adc_cal_raw_to_voltage(reading,&adc_config);
+
+        float voltage2=voltage;
+        voltage2=voltage2/1000;
+        printf("Tensao: %f \n",voltage2);
+        vTaskDelay(100/portTICK_RATE_MS);
     }
     
 
 }
 
-
+/*-------------------Implementação das Funções----------------------*/
 void setup_ADC(){
-    //ADC1 com 12 bits, canal 0(GPIO36) e 0dB de atenuação 
-    adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(ADC1_CHANNEL_0,ADC_ATTEN_DB_0);
-}
+    
+    adc1_config_width(ADC_WIDTH_BIT_12);//ADC1 com 12 bits
 
-void setup_UART(){
-    //feita a configuração UART baseada na documentação da Espressiv: 
-    //https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/uart.html#uart-api-setting-communication-parameters
-   
-    //1)Setting Communication Parameters - Setting baud rate, data bits, stop bits, etc.
-    const int uart_num = UART_NUM_2;
-    uart_config_t uart_config = {
-    .baud_rate = 115200,
-    .data_bits = UART_DATA_8_BITS,
-    .parity = UART_PARITY_DISABLE,
-    .stop_bits = UART_STOP_BITS_1,
-    .flow_ctrl = UART_HW_FLOWCTRL_CTS_RTS,
-    .rx_flow_ctrl_thresh = 122,
-};
-    // Configure UART parameters
-    ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
+    adc1_config_channel_atten(pino_de_leitura,ADC_ATTEN_DB_0);// Canal 5 do ADC1(GPIO33) e 0dB de atenuação 
 
-    //2)Setting Communication Pins - Assigning pins for connection to a device.
-
-    ESP_ERROR_CHECK(uart_set_pin(UART_NUM_0, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, 18, 19));
-
-
-    //3)Driver Installation - Allocating ESP32’s resources for the UART driver.
-
-    // Setup UART buffered IO with event queue
-    const int uart_buffer_size = (1024 * 2);
-    QueueHandle_t uart_queue;
-    // Install UART driver using an event queue here
-    ESP_ERROR_CHECK(uart_driver_install(UART_NUM_2, uart_buffer_size, \
-                                        uart_buffer_size, 10, &uart_queue, 0));
-
-
+    //função para aumentar a precisão na leitura do ADC
+    esp_adc_cal_characterize(ADC_UNIT_1,ADC_ATTEN_DB_0,ADC_WIDTH_BIT_12,defaultVref,&adc_config);
 }
